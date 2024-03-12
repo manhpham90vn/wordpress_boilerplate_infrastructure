4034122cb8d5c0f6d289582d2cd7bdf632704c67
Manh Pham <manhpham90vn@icloud.com>
2024-03-12 17:36:22 +0700

    sync

--- ec2/terraform/acm/main.tf
+++ ec2/terraform/acm/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_acm_certificate" "Certificate_Manager" {
-  private_key = "${file("${path.root}/../certificate/privkey.pem")}"
-  certificate_body = "${file("${path.root}/../certificate/cert.pem")}"
-  certificate_chain = "${file("${path.root}/../certificate/fullchain.pem")}"
+  private_key = "${file("${path.root}/../../certificate/privkey.pem")}"
+  certificate_body = "${file("${path.root}/../../certificate/cert.pem")}"
+  certificate_chain = "${file("${path.root}/../../certificate/fullchain.pem")}"
 }
